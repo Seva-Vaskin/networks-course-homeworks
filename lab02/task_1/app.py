700ea654c9694cdfb5c9f2b8ad1a23c47366dc9f
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-03-01 04:19:57 +0200

    Complete lab02 task 3

--- lab02/task_1/app.py
+++ lab02/task_1/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, jsonify, request
+import os
+
+from flask import Flask, jsonify, request, send_file
 from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 products = dict()
 next_id = 1
@@ -56,6 +64,30 @@ def handle_not_found(error):
     response = jsonify({"message": error.description})
     response.status_code = 404
     return response
+
+
+@app.route('/product/<int:product_id>/image', methods=['POST'])
+def upload_product_icon(product_id):
+    product = products.get(product_id, None)
+    if product is None:
+        raise NotFound(description="Product not found")
+    if 'icon' not in request.files:
+        return jsonify({"message": "No file part"}), 400
+    file = request.files['icon']
+    if file.filename == '':
+        return jsonify({"message": "No selected file"}), 400
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(file_path)
+    product['icon'] = file_path
+    return jsonify(product), 200
+
+
+@app.route('/product/<int:product_id>/image', methods=['GET'])
+def get_product_icon(product_id):
+    product = products.get(product_id, None)
+    if product is None or 'icon' not in product:
+        raise NotFound(description="Product or icon not found")
+    return send_file(product['icon'])
 
 
 if __name__ == '__main__':
